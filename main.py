f45c14d205152fded0c9570cf3f9132788bbb991
pbraer <73088589+pbraer@users.noreply.github.com>
2021-03-03 13:30:00 +0700

    start of inf

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 #               Kokorina D. (%),
 #               Novoselov V. (%)
 
-print('''"""Case-study #4 Парсинг web-страниц
+print('''Case-study #4 Парсинг web-страниц
 Разработчики:
 Браер П.С., Новоселов В.В., Кокорина Д.Е.
 
-"""''')
+''')
 import urllib.request
 
 with open('input.txt','r') as f_in:
@@ -17,6 +17,9 @@ with open('input.txt','r') as f_in:
             f = urllib.request.urlopen(url)
             s = f.read()
             text = str(s)
-            part_name = text.find("nfl-c-player-header__title")
+            part_name = text.find('nfl-c-player-header__title')
             name = text[text.find('>',part_name)+1:text.find('</h1',part_name)]
-            print(name, file = f_out)
+            print(name, file = f_out, end =' '*(20-len(name)))
+            comp_name = text.find('passingCompletions" scope="col"')
+            name_1 = text[text.find('>', comp_name) + 3:text.find('</th>', comp_name) - 22]
+            print(name_1, file = f_out)
