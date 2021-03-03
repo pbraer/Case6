a0204f29c1bb7bcb4937d66b5c4027d20f096ff1
pbraer <73088589+pbraer@users.noreply.github.com>
2021-03-03 10:07:25 +0700

    output file, names

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ print('''"""Case-study #4 Парсинг web-страниц
 Браер П.С., Новоселов В.В., Кокорина Д.Е.
 
 """''')
+import urllib.request
+
+with open('input.txt','r') as f_in:
+    with open('output.txt','w') as f_out:
+        for line in f_in:
+            url = line
+            f = urllib.request.urlopen(url)
+            s = f.read()
+            text = str(s)
+            part_name = text.find("nfl-c-player-header__title")
+            name = text[text.find('>',part_name)+1:text.find('</h1',part_name)]
+            print(name, file = f_out)
