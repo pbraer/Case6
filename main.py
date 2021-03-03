6ff5b05765e0bbeb27423e616a16981fb260a3b4
darya752 <73088666+darya752@users.noreply.github.com>
2021-03-03 05:41:56 +0300

    st1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello')
+# Case-study #4
+# Developers:   Braer P. (%),
+#               Kokorina D. (%),
+#               Novoselov V. (%)
+
+print('''"""Case-study #4 Парсинг web-страниц
+Разработчики:
+Браер П.С., Новоселов В.В., Кокорина Д.Е.
+
+"""''')
+
