70dfc22d4bb4d06512edf242100de0a19beedfe8
pbraer <73088589+pbraer@users.noreply.github.com>
2021-03-03 09:26:29 +0700

    head

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Hello')
+# Case-study #4
+# Developers:   Braer P. (%),
+#               Kokorina D. (%),
+#               Novoselov V. (%)
+
+print('''Case-study #4 Парсинг web-страниц
+Разработчики:
+Браер П.С., Новоселов В.В., Кокорина Д.Е.''')
